b38cadbb0b12f850c61160c8b81b77841d130e0f
Evoreek <wh_961230@163.com>
2025-09-02 15:40:16 +0800

    Logo 提交

--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Event_SceneAUI.cs
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Event_SceneAUI.cs
@@ -1,9 +1,13 @@
+using UnityEngine.UI;
+
 namespace LazyPan {
     public class Behaviour_Event_SceneAUI : Behaviour {
-        private Flow_SceneA flow;
         public Behaviour_Event_SceneAUI (Entity entity, string behaviourSign) : base(entity, behaviourSign) {
-            Flo.Instance.GetFlow(out flow);
-            flow.Login();
+            Flo.Instance.GetFlow(out Flow_SceneA flow);
+            Button jumpBtn = Cond.Instance.Get<Button>(flow.GetUI(), Label.JUMP);
+            ButtonRegister.AddListener(jumpBtn, () => {
+                flow.Next("SceneB");
+            });
         }
 
         public override void DelayedExecute() {
--- Assets/LazyPan/Scripts/GamePlay/Flow/SceneA/Flow_SceneA.cs
+++ Assets/LazyPan/Scripts/GamePlay/Flow/SceneA/Flow_SceneA.cs
@@ -5,14 +5,8 @@ namespace LazyPan {
 		private Comp UI_SceneA;
 
 		private Entity Obj_Camera_Camera;
+		private Entity Obj_Player_Player;
 		private Entity Obj_Event_SceneAUI;
-		private Entity Obj_Event_UserInfo;
-		private Entity Obj_Event_MainInfo;
-		private Entity Obj_Event_LinMou;
-		private Entity Obj_Event_XiuXing;
-		private Entity Obj_Event_XuShi;
-		private Entity Obj_Event_LinLang;
-		private Entity Obj_Event_BoYi;
 
         public override void Init(Flow baseFlow) {
             base.Init(baseFlow);
@@ -20,6 +14,7 @@ namespace LazyPan {
 			UI_SceneA = UI.Instance.Open("UI_SceneA");
 
 			Obj_Camera_Camera = Obj.Instance.LoadEntity("Obj_Camera_Camera");
+			Obj_Player_Player = Obj.Instance.LoadEntity("Obj_Player_Player");
 			Obj_Event_SceneAUI = Obj.Instance.LoadEntity("Obj_Event_SceneAUI");
 
         }
@@ -27,53 +22,6 @@ namespace LazyPan {
 		/*获取UI*/
 		public Comp GetUI() {
 			return UI_SceneA;
-		}
-
-		/*登录*/
-		public void Login() {
-			Obj.Instance.UnLoadEntity(Obj_Event_BoYi);
-			Obj.Instance.UnLoadEntity(Obj_Event_LinLang);
-			Obj.Instance.UnLoadEntity(Obj_Event_XuShi);
-			Obj.Instance.UnLoadEntity(Obj_Event_XiuXing);
-			Obj.Instance.UnLoadEntity(Obj_Event_LinMou);
-			Obj.Instance.UnLoadEntity(Obj_Event_MainInfo);
-			Obj_Event_UserInfo = Obj.Instance.LoadEntity("Obj_Event_UserInfo");
-		}
-
-		/*主面板*/
-		public void Main() {
-			Obj.Instance.UnLoadEntity(Obj_Event_UserInfo);
-			Obj_Event_MainInfo = Obj.Instance.LoadEntity("Obj_Event_MainInfo");
-		}
-
-		/*灵谋信息*/
-		public void LinMou() {
-			Obj.Instance.UnLoadEntity(Obj_Event_LinMou);
-			Obj_Event_LinMou = Obj.Instance.LoadEntity("Obj_Event_LinMou");
-		}
-
-		/*修行信息*/
-		public void XiuXing() {
-			Obj.Instance.UnLoadEntity(Obj_Event_XiuXing);
-			Obj_Event_XiuXing = Obj.Instance.LoadEntity("Obj_Event_XiuXing");
-		}
-
-		/*蓄势信息*/
-		public void XuShi() {
-			Obj.Instance.UnLoadEntity(Obj_Event_XuShi);
-			Obj_Event_XuShi = Obj.Instance.LoadEntity("Obj_Event_XuShi");
-		}
-
-		/*琳琅信息*/
-		public void LinLang() {
-			Obj.Instance.UnLoadEntity(Obj_Event_LinLang);
-			Obj_Event_LinLang = Obj.Instance.LoadEntity("Obj_Event_LinLang");
-		}
-
-		/*博弈信息*/
-		public void BoYi() {
-			Obj.Instance.UnLoadEntity(Obj_Event_BoYi);
-			Obj_Event_BoYi = Obj.Instance.LoadEntity("Obj_Event_BoYi");
 		}
 
 
@@ -85,14 +33,8 @@ namespace LazyPan {
 
         public override void Clear() {
             base.Clear();
-			Obj.Instance.UnLoadEntity(Obj_Event_LinMou);
-			Obj.Instance.UnLoadEntity(Obj_Event_XiuXing);
-			Obj.Instance.UnLoadEntity(Obj_Event_XuShi);
-			Obj.Instance.UnLoadEntity(Obj_Event_LinLang);
-			Obj.Instance.UnLoadEntity(Obj_Event_BoYi);
-			Obj.Instance.UnLoadEntity(Obj_Event_UserInfo);
-			Obj.Instance.UnLoadEntity(Obj_Event_MainInfo);
 			Obj.Instance.UnLoadEntity(Obj_Event_SceneAUI);
+			Obj.Instance.UnLoadEntity(Obj_Player_Player);
 			Obj.Instance.UnLoadEntity(Obj_Camera_Camera);
 
 			UI.Instance.Close("UI_SceneA");
