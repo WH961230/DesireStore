284a61599129ef57f155fc27c23b77c59e8b6346
Evoreek <wh_961230@163.com>
2025-12-22 03:16:31 +0800

    提交

--- Assets/Demo/用户/User.cs
+++
@@ -1,5 +0,0 @@
-﻿[System.Serializable]
-public class User {
-    public string name;
-    public int coin;
-}
--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Event_SceneAUI.cs
+++ Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_Event_SceneAUI.cs
@@ -4,10 +4,6 @@ namespace LazyPan {
     public class Behaviour_Event_SceneAUI : Behaviour {
         public Behaviour_Event_SceneAUI (Entity entity, string behaviourSign) : base(entity, behaviourSign) {
             Flo.Instance.GetFlow(out Flow_SceneA flow);
-            Button jumpBtn = Cond.Instance.Get<Button>(flow.GetUI(), Label.JUMP);
-            ButtonRegister.AddListener(jumpBtn, () => {
-                flow.Next("SceneB");
-            });
         }
 
         public override void DelayedExecute() {
